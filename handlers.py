6d869d3555f3971f6a4446c9309b6ee3dde4560c
MrJuicyBacon <iambacon@yandex.ru>
2019-11-14 01:22:43 +0300

    User orders GET handler added

--- handlers.py
+++ handlers.py
@@ -1,7 +1,7 @@
 import json
 from aiohttp.web import Response, HTTPNotFound
 from models import Session as DbSession
-from models import User
+from models import User, Order, OrderItem, Shop, Book
 
 
 async def users_get_handler(request):
@@ -14,3 +14,56 @@ async def users_get_handler(request):
         user_dict = user.as_dict()
         return Response(body=json.dumps(user_dict, separators=(',', ':')), content_type='application/json')
     raise HTTPNotFound
+
+
+async def users_orders_handler(request):
+    session = DbSession()
+    user_id = request.match_info.get('user_id')
+
+    order_objects = session.query(Order).with_entities(Order.id, Order.reg_date).filter(Order.user_id == user_id)
+    order_ids = [order_id for order_id, _ in order_objects]
+
+    order_item_objects = session.query(OrderItem)\
+        .with_entities(OrderItem.order_id, OrderItem.book_id, OrderItem.shop_id)\
+        .filter(OrderItem.order_id.in_(order_ids))
+    book_ids = set()
+    shop_ids = set()
+
+    for _, book_id, shop_id in order_item_objects:
+        book_ids.add(book_id)
+        shop_ids.add(shop_id)
+
+    book_objects = session.query(Book).filter(Book.id.in_(book_ids))
+    shop_objects = session.query(Shop).filter(Shop.id.in_(shop_ids))
+    books = {}
+    for book_object in book_objects:
+        books[book_object.id] = book_object
+    shops = {}
+    for shop_object in shop_objects:
+        shops[shop_object.id] = shop_object
+
+    order_items_count = {}
+    order_items = []
+    for order_item in order_item_objects:
+        if f'{order_item.order_id}:{order_item.shop_id}:{order_item.book_id}' not in order_items_count:
+            order_items.append(order_item)
+            order_items_count[f'{order_item.order_id}:{order_item.shop_id}:{order_item.book_id}'] = 1
+        else:
+            order_items_count[f'{order_item.order_id}:{order_item.shop_id}:{order_item.book_id}'] += 1
+
+    orders = []
+    for order in order_objects:
+        temp_obj = {
+            'date': order.reg_date.strftime('%Y-%m-%d'),
+            'items': []
+        }
+        for order_item in order_items:
+            if order_item.order_id == order.id:
+                temp_obj['items'].append({
+                    'book': books[order_item.book_id].as_dict(),
+                    'shop': shops[order_item.shop_id].as_dict(),
+                    'quantity': order_items_count[f'{order_item.order_id}:{order_item.shop_id}:{order_item.book_id}'],
+                })
+        orders.append(temp_obj)
+
+    return Response(body=json.dumps({'orders': orders}, separators=(',', ':')), content_type='application/json')
--- sample_api.py
+++ sample_api.py
@@ -1,10 +1,11 @@
 from aiohttp.web import Application, run_app
-from handlers import users_get_handler
+from handlers import users_get_handler, users_orders_handler
 
 
 app = Application()
 
 app.router.add_get(r'/users/{user_id:\d+}', users_get_handler)
+app.router.add_get(r'/users/{user_id:\d+}/orders', users_orders_handler)
 
 if __name__ == '__main__':
     run_app(app)
